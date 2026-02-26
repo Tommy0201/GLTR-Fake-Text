5f666f9f5f954bd8d217f92662b9ec510864b05a
Tommy0201 <ng.phucminh04@gmail.com>
2026-02-26 12:17:48 -0500

    registering model

--- backend/api.py
+++ backend/api.py
@@ -185,7 +185,7 @@ class LM(AbstractLanguageChecker):
         return token
 
 
-# @register_api(name='BERT')
+@register_api(name='BERT')
 class BERTLM(AbstractLanguageChecker):
     def __init__(self, model_name_or_path="bert-base-cased"):
         super(BERTLM, self).__init__()
